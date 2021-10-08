df70bf978eed15b1201354f33744390161fbe03a
Thales Medeiros <85201128+Medeiros92@users.noreply.github.com>
2021-10-07 22:54:01 -0300

    Initial commit

---
+++ ReactCode/src/data/items.ts
@@ -0,0 +1,28 @@
+import { Item } from '../types/Item';
+
+export const items: Item[] = [
+    { 
+        date: new Date(2021, 8, 10),
+        category: 'food',
+        title: 'McDonads',
+        value: 32.12
+    },
+    { 
+        date: new Date(2021, 9, 15),
+        category: 'food',
+        title: 'Burger King',
+        value: 28
+    },
+    { 
+        date: new Date(2021, 9, 16),
+        category: 'rent',
+        title: 'Aluguel',
+        value: 845.65
+    },
+    { 
+        date: new Date(2021, 10, 15),
+        category: 'salary',
+        title: 'Salario',
+        value: 3500
+    }
+];
